4e3b6f4b2207ee62a3f50fe1971a7c87dd4c00fd
luckyleck <luckyleck29@gmail.com>
2023-03-03 14:00:46 -0500

    First redone

--- Arg_Ex.js
+++ Arg_Ex.js
@@ -1,4 +1,4 @@
-function sum() {
+function sum1() {
     let count = 0;
     for (let i = 0; i < arguments.length; i++) {
         count += arguments[i];
@@ -7,4 +7,12 @@ function sum() {
     return count;
 }
 
-console.log(sum(1,2,3,4,5))
+console.log(sum1(1,2,3,4,5))
+
+function sum2(...args) {
+    let count = 0;
+    args.forEach((num) => count += num);
+    return count;
+}
+
+console.log(sum2(1,2,3,4,5))
